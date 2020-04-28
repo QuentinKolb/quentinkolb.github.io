146623e1c3e9501dbcac04a9e9d818e415b80601
QuentinKolb <kol15004@byui.edu>
2020-04-28 07:28:06 -0600

    wehavesomething

---
+++ js/footer.js
@@ -0,0 +1,6 @@
+
+ let year = new Date().getFullYear(); 
+ document.getElementById('year').innerHTML = year;
+ 
+ let date = document.lastModified;
+ document.getElementById('lastUpdate').innerHTML= "Last Updated: " + date;
