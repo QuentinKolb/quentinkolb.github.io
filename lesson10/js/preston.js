0b9af0bec3c843daa479ea2fbd38e0ef63aa61e1
QuentinKolb <kol15004@byui.edu>
2020-06-25 10:16:04 -0600

    letsgooooo

--- lesson10/js/preston.js
+++ lesson10/js/preston.js
@@ -9,21 +9,20 @@ fetch(apiURL1)
         document.getElementById('humid').textContent = "Humidity: " + data.main.humidity + "%";
         document.getElementById('speed').textContent = "Wind Speed: " + data.wind.speed + "mph";
         document.getElementById('current').textContent = "Currently: " + data.weather[0].main;
-        
 
-        const tempNumber = data.main.temp;
-        const speedNumber = data.wind.speed;
-        let wc = 0;
+
+        const tempNumber = Number(data.main.temp);
+        const speedNumber = Number(data.wind.speed);
+        let wc;
 
         if (tempNumber <= 50 && speedNumber > 3) {
-            wc = Math.round(35.74 + (.6215 * tempNumber)-(35.75*Math.pow(speedNumber,.16))+(.4275*tempNumber*Math.pow(speedNumber,.16)));
-            document.getElementById("chill").textContent = wc;
-            } 
-            else{
-                document.getElementById("chill").textContent = "N/A";
-            }
-
-            document.getElementById('chill').textContent = "Wind Chill: " + wc + "\xB0";
+            wc = Math.round(35.74 + (.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, .16)) + (.4275 * tempNumber * Math.pow(speedNumber, .16)));
+        } else {
+            wc = "N/A";
+            console.log(wc);
+        }
+    
+        document.getElementById('chill').textContent = "Wind Chill: " + wc + "\xB0";
 
     });
 
@@ -42,35 +41,37 @@ const weekday = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
 
 const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5604473&units=imperial&APPID=8f3b136cada8556a53f344760a21ddfb";
 
- fetch(apiURL)
+fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
-        
-    let mylist = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
-    console.log(mylist);
+
+        let mylist = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        console.log(mylist);
 
         let forecastDayNumber = todayDayNumber;
-        for (i=0; i<mylist.length; i++) {
-                forecastDayNumber += 1;
-                if (forecastDayNumber === 7){forecastDayNumber=0;}
+        for (i = 0; i < mylist.length; i++) {
+            forecastDayNumber += 1;
+            if (forecastDayNumber === 7) {
+                forecastDayNumber = 0;
+            }
 
-                let theDayName = document.createElement("h3");
-                theDayName.textContent = weekday[forecastDayNumber];
+            let theDayName = document.createElement("h3");
+            theDayName.textContent = weekday[forecastDayNumber];
 
-                let theTemp = document.createElement("p");
-                theTemp.textContent = jsObject.list[i].main.temp + "\xB0";
+            let theTemp = document.createElement("p");
+            theTemp.textContent = jsObject.list[i].main.temp + "\xB0";
 
-                let iconcode = jsObject.list[i].weather[0].icon;
-                let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
-                let theIcon = document.createElement("img");
-                theIcon.src=iconPath;
+            let iconcode = jsObject.list[i].weather[0].icon;
+            let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+            let theIcon = document.createElement("img");
+            theIcon.src = iconPath;
 
-                let theDay = document.createElement("div");
-                theDay.appendChild(theDayName);
-                theDay.appendChild(theTemp);
-                theDay.appendChild(theIcon);
+            let theDay = document.createElement("div");
+            theDay.appendChild(theDayName);
+            theDay.appendChild(theTemp);
+            theDay.appendChild(theIcon);
 
-                document.getElementById('forecast').appendChild(theDay);
-            
+            document.getElementById('forecast').appendChild(theDay);
+
         }
     });
