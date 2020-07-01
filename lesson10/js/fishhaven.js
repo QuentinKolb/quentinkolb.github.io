911e997e60303172852f3b7ef65472fe296566f4
QuentinKolb <kol15004@byui.edu>
2020-07-01 15:50:10 -0600

    boom

---
+++ lesson10/js/fishhaven.js
@@ -0,0 +1,77 @@
+const apiURL1 = "//api.openweathermap.org/data/2.5/weather?id=5605242&units=imperial&APPID=8f3b136cada8556a53f344760a21ddfb";
+
+
+fetch(apiURL1)
+    .then((response) => response.json())
+    .then((data) => {
+        console.log(data);
+        document.getElementById('temp').textContent = "Current temp: " + data.main.temp + "\xB0";
+        document.getElementById('humid').textContent = "Humidity: " + data.main.humidity + "%";
+        document.getElementById('speed').textContent = "Wind Speed: " + data.wind.speed + "mph";
+        document.getElementById('current').textContent = "Currently: " + data.weather[0].main;
+
+
+        const tempNumber = Number(data.main.temp);
+        const speedNumber = Number(data.wind.speed);
+        let wc;
+
+        if (tempNumber <= 50 && speedNumber > 3) {
+            wc = Math.round(35.74 + (.6215 * tempNumber) - (35.75 * Math.pow(speedNumber, .16)) + (.4275 * tempNumber * Math.pow(speedNumber, .16)));
+        } else {
+            wc = "N/A";
+            console.log(wc);
+        }
+    
+        document.getElementById('chill').textContent = "Wind Chill: " + wc + "\xB0";
+
+    });
+
+
+
+
+
+
+
+
+const d = new Date();
+
+const todayDayNumber = d.getDay();
+
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thurs", "Fri", "Sat"];
+
+const apiURL = "//api.openweathermap.org/data/2.5/forecast?id=5605242&units=imperial&APPID=8f3b136cada8556a53f344760a21ddfb";
+
+fetch(apiURL)
+    .then((response) => response.json())
+    .then((jsObject) => {
+
+        let mylist = jsObject.list.filter(x => x.dt_txt.includes('18:00:00'));
+        console.log(mylist);
+
+        let forecastDayNumber = todayDayNumber;
+        for (i = 0; i < mylist.length; i++) {
+            forecastDayNumber += 1;
+            if (forecastDayNumber === 7) {
+                forecastDayNumber = 0;
+            }
+
+            let theDayName = document.createElement("h3");
+            theDayName.textContent = weekday[forecastDayNumber];
+
+            let theTemp = document.createElement("p");
+            theTemp.textContent = jsObject.list[i].main.temp + "\xB0";
+
+            let iconcode = jsObject.list[i].weather[0].icon;
+            let iconPath = "//openweathermap.org/img/w/" + iconcode + ".png";
+            let theIcon = document.createElement("img");
+            theIcon.src = iconPath;
+
+            let theDay = document.createElement("div");
+            theDay.appendChild(theDayName);
+            theDay.appendChild(theTemp);
+            theDay.appendChild(theIcon);
+
+            document.getElementById('forecast').appendChild(theDay);
+
+        }
+    });
